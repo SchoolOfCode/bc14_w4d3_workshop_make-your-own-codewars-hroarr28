e032eeb6ea95eee8be48c2c814c9d5b295e0bb6f
hroarr28 <thcuk1@hotmail.co.uk>
2023-04-10 12:09:24 +0100

    Final version with solution removed

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
 /* 
-👉 Write your kata here!
+Write a function to convert fahrenheit to celsius and round to the nearest whole number.
+the results should display the answer plus a string saying "°C"
 */
 
 //👉 Write the function your CodeWarriors will start with below here:
+
+export function fahrenheitToCelsius(fahrenheit) {
+    //Write your code here
+}  
+
--- main.test.js
+++ main.test.js
@@ -1,1 +1,14 @@
 //👉 Write your tests below here:
+
+
+import {jest} from '@jest/globals';
+
+import { fahrenheitToCelsius } from "./main.js";
+
+test( "fahrenheit to celsius converter ", () => {
+    expect(fahrenheitToCelsius(32)).toBe("0°C");
+    expect(fahrenheitToCelsius(50)).toBe("10°C");
+    expect(fahrenheitToCelsius(212)).toBe("100°C");
+    expect(fahrenheitToCelsius(0)).toBe("-18°C");
+    
+});
